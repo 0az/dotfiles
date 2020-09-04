c5153a252edc2a1909aa7794e0d11b7f095b7402
0az <0az@afzhou.com>
2020-09-04 15:08:52 -0500

    fix(fish): Fix source-env

--- fish/.config/fish/functions/source-env.fish
+++ fish/.config/fish/functions/source-env.fish
@@ -1,12 +1,8 @@
 function source-env --description 'Read environment variables from .env files'
 	for f in $argv;
-		sed -E '/^#/d' "$f" \
-		| begin
-			while read line
-				if ! export "$line"
-					exit 1
-				end
-			end
-		end
+		export (
+			sed -E '/^#/d' "$f" \
+			| xargs -L 1
+		)
 	end
 end
