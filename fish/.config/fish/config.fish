42e209407d58cf3462c574b656f845c89a987297
0az <0az@afzhou.com>
2021-06-24 11:25:59 -0500

    feat(fish): Implement conditional homebrew completions

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,2 +1,12 @@
 source ~/.config/shell/profile
 source ~/.config/shell/aliases
+
+if command -q brew
+	if test -d (brew --prefix)"/share/fish/completions"
+		set -gx fish_complete_path $fish_complete_path (brew --prefix)/share/fish/completions
+	end
+
+	if test -d (brew --prefix)"/share/fish/vendor_completions.d"
+		set -gx fish_complete_path $fish_complete_path (brew --prefix)/share/fish/vendor_completions.d
+	end
+end
