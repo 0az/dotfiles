3785360af6e44b17745e580e72cd9cc95bb03949
0az <0az@afzhou.com>
2022-02-22 18:16:31 -0800

    minor(fish): Fix source-env

--- fish/.config/fish/functions/source-env.fish
+++ fish/.config/fish/functions/source-env.fish
@@ -1,8 +1,10 @@
 function source-env --description 'Read environment variables from .env files'
-	for f in $argv;
-		export (
-			sed -E '/^#/d' "$f" \
+	for f in $argv
+		for v in (
+			sed -E -e '/^#/d' -e 's/^export //' "$f" \
 			| xargs -L 1
 		)
+			export $v
+		end
 	end
 end
