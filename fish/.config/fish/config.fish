778e378051cc20219e81949a475fe7578517e90c
0az <0az@afzhou.com>
2022-04-22 18:29:44 -0700

    fish: Only invoke (brew --prefix) once

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,11 +2,12 @@ source ~/.config/shell/profile
 source ~/.config/shell/aliases
 
 if command -q brew
-	if test -d (brew --prefix)"/share/fish/completions"
-		set -gx fish_complete_path $fish_complete_path (brew --prefix)/share/fish/completions
+	set -l brew_prefix (brew --prefix)
+	if test -d "$brew_prefix/share/fish/completions"
+		set -gx fish_complete_path $fish_complete_path $brew_prefix/share/fish/completions
 	end
 
-	if test -d (brew --prefix)"/share/fish/vendor_completions.d"
-		set -gx fish_complete_path $fish_complete_path (brew --prefix)/share/fish/vendor_completions.d
+	if test -d "$brew_prefix/share/fish/vendor_completions.d"
+		set -gx fish_complete_path $fish_complete_path $brew_prefix/share/fish/vendor_completions.d
 	end
 end
