1e1b711ee6612d9c563a17b5bbb5e0d2e3a52237
0az <0az@afzhou.com>
2022-06-07 14:55:53 -0500

    fish: Only load aliases in interactive sessions

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,13 +1,16 @@
 source ~/.config/shell/profile
-source ~/.config/shell/aliases
 
-if status is-interactive && command -q brew
-	set -l brew_prefix (brew --prefix)
-	if test -d "$brew_prefix/share/fish/completions"
-		set -gx fish_complete_path $fish_complete_path $brew_prefix/share/fish/completions
-	end
+if status is-interactive
+	source ~/.config/shell/aliases
 
-	if test -d "$brew_prefix/share/fish/vendor_completions.d"
-		set -gx fish_complete_path $fish_complete_path $brew_prefix/share/fish/vendor_completions.d
+	if command -q brew
+		set -l brew_prefix (brew --prefix)
+		if test -d "$brew_prefix/share/fish/completions"
+			set -gx fish_complete_path $fish_complete_path $brew_prefix/share/fish/completions
+		end
+
+		if test -d "$brew_prefix/share/fish/vendor_completions.d"
+			set -gx fish_complete_path $fish_complete_path $brew_prefix/share/fish/vendor_completions.d
+		end
 	end
 end
