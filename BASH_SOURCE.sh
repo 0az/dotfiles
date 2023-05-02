18d9d38a7780a8df5166467207bb31f734e33a68
0az <0az@afzhou.com>
2023-05-02 14:04:44 -0700

    meta: Record the "bash script directory" snippet for posterity

---
+++ BASH_SOURCE.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+# This snippet's useful, so it gets to live in dotfiles.
+
+# shellcheck disable=SC2034
+root="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd || exit 1)"
