ac91aa978a19ebcff07a7f10232fc4192c90edc9
0az <0az@afzhou.com>
2023-08-27 00:42:21 -0700

    fish: Update source-env with support for arbitrary shell commands

--- fish/.config/fish/functions/source-env.fish
+++ fish/.config/fish/functions/source-env.fish
@@ -1,10 +1,50 @@
 function source-env --description 'Read environment variables from .env files'
-	for f in $argv
-		for v in (
-			sed -E -e '/^#/d' -e 's/^export //' "$f" \
-			| xargs -L 1
-		)
-			export $v
-		end
-	end
+    argparse 'n/dry-run' -- $argv
+
+    set -l shell
+    if ! set shell (command -s dash)
+        set shell (command -s sh)
+    end
+
+    for f in $argv
+        if ! dash -n "$f" && grep -Eqvx "\w+=.+|\s+" "$f"
+            echo "Invalid env file: $f" >&2
+            return 1
+        end
+    end
+
+    diff -u0 \
+        (env | sort | psub) \
+        (
+            dash -eu -c '
+                for __word in "$@"; do
+                    if test "${__word#*/}" = "$__word"; then
+                        __word=./"$__word"
+                    fi
+                    set -a
+                    . $__word
+                    set +a
+                done
+                env
+            ' -- $argv \
+            | sort | psub \
+        ) \
+    | if test -z "$_flag_dry_run"
+        sed -E -n '
+            3,${
+                /^-|^@@/d
+                s/^\\+/export /
+                p
+            }
+        ' | source
+    else
+        sed -E -n '
+            3,${
+                /^-|^@@/d
+                s/^\\+//
+                p
+            }
+        '
+    end
+
 end
