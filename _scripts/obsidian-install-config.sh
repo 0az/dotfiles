c8da858f9967276444886b623a989428d8c03356
0az <0az@afzhou.com>
2023-10-14 19:53:24 -0700

    obsidian: Fix config install script

--- _scripts/obsidian-install-config.sh
+++ _scripts/obsidian-install-config.sh
@@ -4,9 +4,13 @@ set -euo pipefail
 
 cwd_name=${PWD##*/}
 cwd_name=${cwd_name:-/}
-debug="${DEBUG:-}"
 
 obsidian_common="${XDG_DATA_HOME:-$HOME/.local/share}/obsidian-common"
+
+if ! test -d "$obsidian_common"; then
+	echo 'Could not find obsidian-common directory. Did you remember to link stow it?' >&2
+	exit 1
+fi
 
 if test "$cwd_name" != '.obsidian'; then
 	if ! test -d .obsidian/; then
@@ -20,15 +24,22 @@ for f in "$obsidian_common"/*; do
 	name="${f##*/}"
 	# shellcheck disable=SC2015
 	if test -e "$name" -a ! -L "$name"; then
-			errors+=("$name")
+		errors+=("$name")
 	fi
 done
+
+if command -v delta >/dev/null; then
+	diff() { delta --paging never "$@"; }
+else
+	diff() { command diff -u "$@"; }
+fi
 
 if test "${#errors[@]}" -ne 0; then
 	echo 'Encountered conflicting files!' >&2
 	for name in "${errors[@]}"; do
-		file --no-dereference "$name"
-	done | sed "s@$HOME@~@" >&2
+		file --no-dereference "$name" | sed "s@$HOME@~@"
+		diff "$name" "$obsidian_common/$name" || true
+	done
 	exit 1
 fi
 
