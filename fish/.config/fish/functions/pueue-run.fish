61d3778acf0628b77cac1afb7203093d2e9fdd30
0az <0az@afzhou.com>
2024-01-30 12:08:23 -0800

    fish: Add pueue helpers

---
+++ fish/.config/fish/functions/pueue-run.fish
@@ -0,0 +1,6 @@
+function pueue-run
+    test -n "$argv" || return 1
+    set -l task_id (pueue add --print-task-id $argv) || return $status
+    pueue wait --status Running $task_id
+    pueue follow $task_id
+end
