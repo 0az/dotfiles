2499862ae24d75f7d0129fb3139f21e3554aea25
0az <0az@afzhou.com>
2024-02-13 22:56:53 -0800

    fish: Extract common profile and Homebrew support

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,19 +1,5 @@
-source ~/.config/shell/profile
+# NOTE: The common shell profile is loaded from ./conf.d/000-profile.fish
 
 if status is-interactive
 	source ~/.config/shell/aliases
-	mkdir -p $XDG_CACHE_HOME/dotfiles
-
-	if command -q brew
-		if test -z "$brew_prefix" -o ! -d "$brew_prefix"
-			set -U brew_prefix (brew --prefix)
-		end
-		if test -d "$brew_prefix/share/fish/completions"
-			set -gx fish_complete_path $fish_complete_path $brew_prefix/share/fish/completions
-		end
-
-		if test -d "$brew_prefix/share/fish/vendor_completions.d"
-			set -gx fish_complete_path $fish_complete_path $brew_prefix/share/fish/vendor_completions.d
-		end
-	end
 end
