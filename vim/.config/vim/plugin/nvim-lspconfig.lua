ed29362e28301590399ede60a6b18d629d51e4b3
0az <0az@afzhou.com>
2024-04-08 12:24:49 -0700

    vim: Add extra <leader>e keybind for LSP diagnostics in neovim

--- vim/.config/vim/plugin/nvim-lspconfig.lua
+++ vim/.config/vim/plugin/nvim-lspconfig.lua
@@ -32,6 +32,7 @@ vim.diagnostic.config {
 -- Global mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
 vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
 vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
