c2ceeff17658da25aa3018cb2225b27241634b07
0az <0az@afzhou.com>
2024-11-23 00:45:02 -0800

    fish: Gracefully fail when asdf.fish doesn't exist

--- fish/.config/fish/conf.d/asdf.fish
+++ fish/.config/fish/conf.d/asdf.fish
@@ -4,6 +4,8 @@ if status is-interactive && test -n "$ASDF_CONFIG_FILE" -a -n "$ASDF_DATA_DIR" -
     set asdf_dir $brew_prefix/opt/asdf
     set -p fish_complete_path $asdf_dir/share/fish/vendor_completions.d
   end
-  source $asdf_dir/libexec/asdf.fish
+  set -l asdf_script $asdf_dir/libexec/asdf.fish
+  test -e $asdf_script || return
+  source $asdf_script
   set -a fish_complete_path $asdf_dir/completions
 end
