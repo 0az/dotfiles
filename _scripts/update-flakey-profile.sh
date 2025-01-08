2a422c8e1fe9c5731b872e03d201f864a2c65801
0az <0az@afzhou.com>
2025-01-08 12:54:34 -0800

    scripts: Add update-flakey-profile script for Nix profile rebuilds

---
+++ _scripts/update-flakey-profile.sh
@@ -0,0 +1,11 @@
+#! /usr/bin/env bash
+
+set -euo pipefail
+
+root="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." >/dev/null 2>&1 && pwd || exit 1)"
+. "$root/_scripts/_common.sh"
+
+flake="path:$root/nix/dot-config/flakey-profile"
+
+nix flake update nixpkgs --flake "$flake"
+nix run "$flake#profile.switch"
