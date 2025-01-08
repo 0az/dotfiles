cff1ace0a1209bccbc31798e811cf92551d52eed
0az <0az@afzhou.com>
2025-01-08 11:38:25 -0800

    shell: Clean up BASH_SOURCE formatting

--- shell/.config/shell/bin/BASH_SOURCE.sh
+++ shell/.config/shell/bin/BASH_SOURCE.sh
@@ -2,6 +2,6 @@
 # This snippet's useful, so it gets to live in dotfiles.
 
 # shellcheck disable=SC2034
-root="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd || exit 1)"
+root="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd || exit 1)"
 
 sed -n '5p' "${BASH_SOURCE[0]}"
