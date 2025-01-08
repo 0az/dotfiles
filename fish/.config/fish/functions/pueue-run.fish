33a4dcb21ea2628bbacb8490a71782134f4014b2
0az <0az@afzhou.com>
2025-01-08 11:37:41 -0800

    fish: Print the actual `pueue add` command in pueue-run

--- fish/.config/fish/functions/pueue-run.fish
+++ fish/.config/fish/functions/pueue-run.fish
@@ -1,5 +1,6 @@
 function pueue-run
     test -n "$argv" || return 1
+    echo pueue add --print-task-id $argv >&2
     set -l task_id (pueue add --print-task-id $argv) || return $status
     pueue wait --status Running $task_id
     pueue follow $task_id
