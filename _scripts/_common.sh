0afaa1f663ad7fe872ba4bbbcdb21b3c49c52472
0az <0az@afzhou.com>
2025-01-10 01:35:36 -0800

    scripts: Conditionally set -e before sourcing nix-daemon.sh in _common.sh

--- _scripts/_common.sh
+++ _scripts/_common.sh
@@ -1,5 +1,15 @@
 #! /usr/bin/env bash
 
 nix_daemon_profile=/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
+
+_opts=$-
+if [[ $- =~ e ]]; then
+	set +e
+fi
+
 # shellcheck disable=SC1090
 test -e "$nix_daemon_profile" && . "$nix_daemon_profile"
+
+if [[ $_opts =~ e ]]; then
+	set -e
+fi
