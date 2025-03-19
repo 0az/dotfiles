7cd4ab64614745f52789d5cf9d608625526cb727
0az <0az@afzhou.com>
2025-03-18 22:55:33 -0700

    fish: Add abbreviations

---
+++ fish/.config/fish/conf.d/abbr.fish
@@ -0,0 +1,17 @@
+abbr -a nv nvim
+
+abbr -a nextest 'cargo nextest'
+
+abbr -a py python
+abbr -a py python3
+
+abbr -a ptipy ptipython
+abbr -a ptpy ptpython
+
+abbr -a gco 'git checkout'
+abbr -a gfi 'git commit --fixup HEAD'
+abbr -a gri 'git rebase -i'
+
+abbr -a gbl 'git branchless'
+abbr -a gbh 'git branchless help'
+abbr -a gsl 'git branchless smartlog'
