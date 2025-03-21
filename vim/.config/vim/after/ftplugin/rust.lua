1157dca7f62a48b9ac04a726026123211d21f237
0az <0az@afzhou.com>
2025-03-21 14:43:41 -0700

    vim: Only apply Rustaceanvim mappings if plugin is loaded

--- vim/.config/vim/after/ftplugin/rust.lua
+++ vim/.config/vim/after/ftplugin/rust.lua
@@ -4,7 +4,3 @@ local buf_opts = { buffer = bufnr }
 local silent_buf_opts = vim.tbl_extend('error', buf_opts, { silent = true })
 
 -- Action
-vim.keymap.set('n', '<leader>a', function()
-	vim.cmd.RustLsp 'codeAction' -- supports rust-analyzer's grouping
-	-- or vim.lsp.buf.codeAction() if you don't want grouping.
-end, silent_buf_opts)
--- vim/.config/vim/ftplugin/rust.lua
+++ vim/.config/vim/ftplugin/rust.lua
@@ -3,34 +3,36 @@ local bufnr = vim.api.nvim_get_current_buf()
 
 local opts = { buffer = bufnr }
 
-vim.keymap.set({ 'n', 'v' }, 'J', function()
-	vim.cmd.RustLsp 'joinLines'
-end, opts)
+if vim.g.loaded_rustaceanvim then
+	vim.keymap.set({ 'n', 'v' }, 'J', function()
+		vim.cmd.RustLsp 'joinLines'
+	end, opts)
 
-vim.keymap.set({ 'n', 'v' }, '<space>ca', function()
-	vim.cmd.RustLsp 'codeAction'
-end, opts)
+	vim.keymap.set({ 'n', 'v' }, '<space>ca', function()
+		vim.cmd.RustLsp 'codeAction'
+	end, opts)
 
-vim.keymap.set('n', '<C-k>d', function()
-	vim.cmd.RustLsp 'debuggables'
-end, opts)
+	vim.keymap.set('n', '<C-k>d', function()
+		vim.cmd.RustLsp 'debuggables'
+	end, opts)
 
-vim.keymap.set('n', '<C-k>D', function()
-	vim.cmd.RustLsp 'debug'
-end, opts)
+	vim.keymap.set('n', '<C-k>D', function()
+		vim.cmd.RustLsp 'debug'
+	end, opts)
 
-vim.keymap.set('n', '<C-k>r', function()
-	vim.cmd.RustLsp 'runnables'
-end, opts)
+	vim.keymap.set('n', '<C-k>r', function()
+		vim.cmd.RustLsp 'runnables'
+	end, opts)
 
-vim.keymap.set('n', '<C-k>R', function()
-	vim.cmd.RustLsp 'run'
-end, opts)
+	vim.keymap.set('n', '<C-k>R', function()
+		vim.cmd.RustLsp 'run'
+	end, opts)
 
-vim.keymap.set('n', '<C-k>t', function()
-	vim.cmd.RustLsp 'testables'
-end, opts)
+	vim.keymap.set('n', '<C-k>t', function()
+		vim.cmd.RustLsp 'testables'
+	end, opts)
 
-vim.keymap.set('n', '<C-k>T', function()
-	vim.cmd.RustLsp { 'testables', bang = true }
-end, opts)
+	vim.keymap.set('n', '<C-k>T', function()
+		vim.cmd.RustLsp { 'testables', bang = true }
+	end, opts)
+end
