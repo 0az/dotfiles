c5d73e2763965a9b692f1d4aba620b9040c92fd8
0az <0az@afzhou.com>
2025-10-02 15:29:20 -0700

    fish: Fix pueue-last-running-in-group

--- fish/.config/fish/functions/pueue-last-running-in-group.fish
+++ fish/.config/fish/functions/pueue-last-running-in-group.fish
@@ -2,5 +2,5 @@ function pueue-last-running-in-group --argument group
 	if test -z "$group"
 		return 1
 	end
-	pueue status --json | jq -Mr '.tasks | to_entries | map(select(.value.group == "'"$group"'" and .value.status == "Running").key) | max // halt_error'
+	pueue status --json | jq -Mr '.tasks | to_entries | map(select(.value.group == "'"$group"'" and .value.status.Running != null).key) | max // halt_error'
 end
