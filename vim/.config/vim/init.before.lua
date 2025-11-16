388222101d5225a90bbc87b965e2df10bb105cf0
0az <0az@afzhou.com>
2025-11-16 02:39:54 -0800

    vim: Add try_require lua global

--- vim/.config/vim/init.before.lua
+++ vim/.config/vim/init.before.lua
@@ -1,1 +1,10 @@
 require('before.rustaceanvim').setup()
+
+_G.try_require = function(lib)
+	local ok, result = pcall(require, lib)
+	if not ok then
+		vim.notify_once('try_require: failed to require ' .. lib, vim.log.levels.WARN, {})
+		return nil
+	end
+	return result
+end
