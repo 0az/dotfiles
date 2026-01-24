ba351f14d9c4731c6923cd6d4103e06f5fde47c1
0az <0az@afzhou.com>
2026-01-24 10:59:19 -0800

    scripts: Add setup-fish.sh script

---
+++ _scripts/setup-fish.sh
@@ -0,0 +1,8 @@
+#! /usr/bin/env bash
+
+# This script MUST run before the first fish shell invocation for the account
+
+set -euo pipefail
+
+mkdir -p ~/.cache ~/.config ~/.local/share/fish
+# touch ~/.local/share/fish/fish_variables
