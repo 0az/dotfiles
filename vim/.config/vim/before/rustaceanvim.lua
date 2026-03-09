594593ce76c85ffbd88a41fad7c30f674ce18e6e
0az <0az@afzhou.com>
2026-03-09 02:12:48 -0700

    vim: Update rustaceanvim configuration

--- vim/.config/vim/before/rustaceanvim.lua
+++ vim/.config/vim/before/rustaceanvim.lua
@@ -1,15 +1,26 @@
 local M = {}
 
 function M.setup()
-	vim.g.rustaceanvim = function()
-		local rust_hooks = try_require 'local.ftplugin-hooks.rust' or {}
+	local rust_hooks = try_require 'local.ftplugin-hooks.rust' or {}
 
+	vim.g.rustaceanvim = function()
 		local config = {
-			server = {},
+			server = {
+				handlers = {
+					['window/logMessages'] = function(err, result, ctx, config)
+						if result and result.message:find 'overly long loop' then
+							return
+						end
+						vim.lsp.handlers['window/logMessage'](err, result, ctx, config)
+					end,
+				},
+			},
 			tools = {
 				code_actions = {
 					ui_select_fallback = true,
 				},
+				enable_nextest = true,
+				enable_clippy = true,
 				test_executor = 'background',
 			},
 		}
